87460e2354c081fb920d272ae8d01d615d528fa0
Vladislav <ryabov.it@gmail.com>
2022-11-14 23:02:47 +0200

    contacts page

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,4 +1,10 @@
 "use strict";
 
-console.log("test");
+document.querySelector('#btn1').onclick = function () {
+  document.querySelector('.home-page__modal-container').classList.add('is-visible');
+};
+
+document.querySelector('.home-page__modal-close').onclick = function () {
+  document.querySelector('.home-page__modal-container').classList.remove('is-visible');
+};
 //# sourceMappingURL=index.js.map
