63af4afe8f315119ac50fb35aafa541e059deba9
Vladislav <ryabov.it@gmail.com>
2022-11-22 00:24:41 +0200

    blog

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,10 +1,31 @@
 "use strict";
 
-document.querySelector('#btn1').onclick = function () {
-  document.querySelector('.home-page__modal-container').classList.add('is-visible');
+try {
+  document.querySelector('#btn1').onclick = function () {
+    document.querySelector('.home-page__modal-container').classList.add('is-visible');
+  };
+
+  document.querySelector('.home-page__modal-close').onclick = function () {
+    document.querySelector('.home-page__modal-container').classList.remove('is-visible');
+  };
+} catch (_unused) {
+  console.log("dont find");
+}
+
+console.log("test");
+var h1 = document.querySelectorAll('.animation');
+var active = 'animation__active';
+
+var isInViewport = function isInViewport(elem) {
+  var bounding = elem.getBoundingClientRect();
+  return bounding.top >= 0 && bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight);
 };
 
-document.querySelector('.home-page__modal-close').onclick = function () {
-  document.querySelector('.home-page__modal-container').classList.remove('is-visible');
-};
+addEventListener('scroll', function (event) {
+  for (var i = 0; i < h1.length; i++) {
+    if (isInViewport(h1[i])) {
+      h1[i].classList.add(active);
+    }
+  }
+});
 //# sourceMappingURL=index.js.map
--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,9 +1,35 @@
 
+try {
+    document.querySelector('#btn1').onclick = () => {
+        document.querySelector('.home-page__modal-container').classList.add('is-visible')
+    }
+    
+    document.querySelector('.home-page__modal-close').onclick = () => {
+        document.querySelector('.home-page__modal-container').classList.remove('is-visible')
+    }
+} 
 
-document.querySelector('#btn1').onclick = () => {
-    document.querySelector('.home-page__modal-container').classList.add('is-visible')
-}
+catch {console.log("dont find")}
 
-document.querySelector('.home-page__modal-close').onclick = () => {
-    document.querySelector('.home-page__modal-container').classList.remove('is-visible')
-}
+
+
+console.log("test");
+
+let h1 = document.querySelectorAll('.animation');
+let active = 'animation__active';
+
+let isInViewport = function (elem) {
+    let bounding = elem.getBoundingClientRect();
+    return (
+        bounding.top >= 0 &&
+        bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+    );
+};
+
+addEventListener('scroll', (event) => {
+    for (let i = 0; i < h1.length; i++) {
+        if (isInViewport(h1[i])) {
+            h1[i].classList.add(active);
+        }
+    }
+});
